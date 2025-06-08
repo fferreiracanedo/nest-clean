f1830f1e19d82a1814a936cb5cb592feac9ded48
Mateus Ferreira <mateusferreiracanedo@gmail.com>
2025-06-08 17:28:52 -0300

    feat: adiciona suporte à configuração de ambiente com @nestjs/config e validação usando Zod

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateAccountService } from './services/create-account.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
+  imports: [ConfigModule.forRoot({
+    validate : env => envSchema.parse(env),
+    isGlobal: true,
+  })
+],
   controllers: [CreateAccountController],
   providers: [PrismaService, CreateAccountService],
 })
