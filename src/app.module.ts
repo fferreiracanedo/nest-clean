a32ed6f70b497b3fd969cb14ddeb5747787890d5
Mateus Ferreira <mateusferreiracanedo@gmail.com>
2025-06-08 20:19:44 -0300

    feat: adiciona suporte a autenticação com JWT e Passport, além de atualizar dependências e configurar chaves privadas no .gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { CreateAccountService } from './services/create-account.service'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [ConfigModule.forRoot({
     validate : env => envSchema.parse(env),
     isGlobal: true,
-  })
+  }),
+  AuthModule,
 ],
   controllers: [CreateAccountController],
   providers: [PrismaService, CreateAccountService],
